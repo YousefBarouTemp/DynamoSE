5bc6896e62caa1fc41683d8ecdfe7df6c464f13f
Yousef360 <yousef.elbaroudy02@gmail.com>
2024-05-11 05:28:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Email: yousef.elbaroudy02@gmail.com"""})
 
 c1, c2, c3 = st.columns([1,30,1])
 with c2:
+    st.title("Dynamo Search Engine")
     st.image('photos/bfcai.png', caption="Faculty of Computers and Artificial Intelligence")
 
 if "my_engine" not in st.session_state:
@@ -44,11 +45,11 @@ with tab2:
                     st.write(result[i])
                     
                     
-                    
-cc1, cc2, cc3 = st.columns([1,10,1])
-with cc2:
-    st.header("Created by:")
-    st.markdown('''
-    :red[Yousef Elbaroudy]
+with st.container(border=True)
+    cc1, cc2, cc3 = st.columns([1,10,1])
+    with cc2:
+        st.header("Created by:")
+        st.markdown('''
+        :red[Yousef Elbaroudy]
 
-    Email: yousef.elbaroudy02@gmail.com''')
+        Email: yousef.elbaroudy02@gmail.com''')
